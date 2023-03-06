2bc96abe220e36ea291a3702d22c464bdd9d03b2
konedi <mejiae1994@gmail.com>
2023-03-06 00:28:25 -0500

    playing around with another layout

--- resources/app.js
+++ resources/app.js
@@ -3,17 +3,31 @@ const elements = document.querySelectorAll(".project-article");
 const nextSlide = document.querySelector(".btn-next");
 const projectImg = document.querySelectorAll(".project-img");
 const githubIcon = document.getElementById("github-link");
+const demoLink = document.getElementById("demo-link");
 const progressItems = document.querySelectorAll(".progress-item");
 const modal = document.getElementById("myModal");
 const modalImg = document.getElementById("img01");
 const span = document.getElementsByClassName("close")[0];
 
-const imageUrlList = ["http://google.com", "http://youtube.com", "http://google.com"];
+const projectSources = [
+  {
+    githubLink: "https://github.com/mejiae1994/Eel",
+    demoLink: "https://mejiae1994.github.io/Eel/"
+  },
+  {
+    githubLink: "http://google.com",
+    demoLink: "http://youtube.com"
+  },
+  {
+    githubLink: "http://youtube.com",
+    demoLink: "http://google.com"
+  }
+]
 
 let curSlide = 0;
 let maxSlide = elements.length - 1;
 
-hideExceptCurrent();
+// hideExceptCurrent();
 
 function hideExceptCurrent() {
   for (let i = 0; i < elements.length; i++) {
@@ -40,7 +54,8 @@ for (let i = 0; i < projectImg.length; i++) {
   projectImg[i].addEventListener("click", () => {
     modal.style.display = "block";
     modalImg.src = projectImg[i].src;
-    githubIcon.href = imageUrlList[i];
+    githubIcon.href = projectSources[i].githubLink;
+    demoLink.href = projectSources[i].demoLink;
   })
 }
 
